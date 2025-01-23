05ade9244451662437364017124289f2bfa38c52
Marcelo henrique <120139095+marceloteck@users.noreply.github.com>
2025-01-23 07:22:26 -0300

    update diretorio

--- MEGACOD.cmd
+++ MEGACOD.cmd
@@ -3,7 +3,7 @@ rem Ativar o ambiente virtual
 call Scripts\activate
 
 rem Executar o script Python
-python MEGA-SENA\Mega_cod1.1.6.py
+python MEGA-SENA\Mega_cod_1.2.1.py
 
 rem Manter a janela do terminal aberta
 pause
